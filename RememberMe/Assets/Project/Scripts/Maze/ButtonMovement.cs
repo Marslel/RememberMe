53b0147af4a635812173da7b574c2031ffca1306
LisaJaksties <lisa.jaksties.office@gmail.com>
2023-11-16 23:31:38 +0100

    Update Maze
    + move wall with button collision
    + walls not seethrough

---
+++ RememberMe/Assets/Project/Scripts/Maze/ButtonMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovement : MonoBehaviour
+{
+
+    [SerializeField] 
+    GameObject button;
+    [SerializeField] 
+    Collider wall;
+    [SerializeField] 
+    Animation anim;
+
+    UnityEngine.Vector3 buttonPos;
+
+    bool activated;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other != wall && !activated){
+            activated = true;
+            anim.Play("wall_goes_down");
+            //buttonPos = new UnityEngine.Vector3(73.9f,-67.81284f,3.069515f);
+            button.transform.position = buttonPos;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("down")){
+            anim.Play("wall_goes_down");
+        }
+    }
+}
