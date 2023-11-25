50c2c5a6bbff11338f01dc3b1831271562da820c
LisaJaksties <lisa.jaksties.office@gmail.com>
2023-11-25 20:40:05 +0100

    repair blackscreen, teleport after NPC interaction

--- RememberMe/Assets/Project/Scripts/Update_Maze_Status.cs
+++ RememberMe/Assets/Project/Scripts/Update_Maze_Status.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Update_Game_Status : MonoBehaviour
+public class Update_Maze_Status : MonoBehaviour
 {
-
+     [SerializeField]
     public Data_Storage data_Storage;
+    public GameObject exit;
+
     // Start is called before the first frame update
     void Start()
     {
-        if(data_Storage != null){
-            data_Storage.updateMazeWon(true);
-            data_Storage.updatePuzzlesSolved(1);
-        }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Puzzles Solved: "+ data_Storage.puzzlesSolved);
     }
+
+    private void OnTriggerEnter(Collider other){
+
+        if(other.tag == "Player"){
+
+            if(data_Storage != null){
+                data_Storage.updateMazeWon(true);
+                data_Storage.updatePuzzlesSolved(1);
+            }
+            exit.GetComponent<HideObject>().makeVisible();
+        }  
+    }
+
 }
