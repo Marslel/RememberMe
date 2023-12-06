6f05c74d2fe87363c2f6275b40ae013a0e19171d
Lena Jaksties <lena.jaksties.work@gmail.com>
2023-12-06 17:06:52 +0100

    Cyber Sickness walk

--- RememberMe/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ RememberMe/Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 Run
+        {
+            get
+            {
+                return SteamVR_Actions.default_Run;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
