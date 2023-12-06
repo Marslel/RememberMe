2f9188881b19657da30c798d0260ae2cc486dde8
LisaJaksties <lisa.jaksties.office@gmail.com>
2023-12-07 00:33:30 +0100

    Exit Menu follows Player Camera by pressing A

--- RememberMe/Assets/Project/Scripts/ShowExitScreen.cs
+++ RememberMe/Assets/Project/Scripts/ShowExitScreen.cs
@@ -10,16 +10,21 @@ public class ShowExitScreen : MonoBehaviour
     private bool menuVisible;
     public GameObject exitText;
     public GameObject exitButton;
+    GameObject player;
+
+    public Camera mainCamera;
+    public GameObject menuScreen;
     // Start is called before the first frame update
     void Start()
     {
        handType = SteamVR_Input_Sources.RightHand;
+       player =  GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         // Change the ButtonMask to access other inputs
         if (buttonMenu.GetStateDown(handType))
         {
@@ -27,6 +32,13 @@ public class ShowExitScreen : MonoBehaviour
             Debug.Log("Trigger down");
 
             if(menuVisible){
+                //set location of menu in front of player coordinates
+                //Vector3 distanceToPlayer = new Vector3(0.5f,1,0);
+                menuScreen.transform.position = mainCamera.transform.forward + mainCamera.transform.position;
+                //menuScreen.transform.LookAt(player.transform.position + mainCamera.transform.rotation * Vector3.forward, mainCamera.transform.rotation * Vector3.up);
+                Vector3 relativePos = mainCamera.transform.position - menuScreen.transform.position;
+                menuScreen.transform.rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+
                 exitText.SetActive(true);
                 exitButton.SetActive(true);
             }else{
