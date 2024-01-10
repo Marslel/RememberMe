cec5bb305aaaf159247bc377be97bfe530d22e54
Marslel <marcel.bergen123@gmail.com>
2024-01-10 11:22:44 +0100

    add Voice Lines to Character && fix Bugs

---
+++ RememberMe/Assets/Project/Scripts/VoiceLines.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceLines : MonoBehaviour
+{
+    public AudioClip voiceLine;
+
+    private AudioSource audioSource;
+
+    public string[] dialog;
+    void Start()
+    {
+        // AudioSource-Komponente abrufen oder hinzufügen
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        // Voice Line zuweisen
+        audioSource.clip = voiceLine;
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Head")){
+            audioSource.Play();
+        }
+    }
+}
