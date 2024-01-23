9c49da92fc623f6cdf205e582e6ac9ea21434490
LisaJaksties <lisa.jaksties.office@gmail.com>
2024-01-24 00:48:48 +0100

    update Ui and Map in Maze

--- RememberMe/Assets/Project/Scripts/Maze/UpdateMazeLevel.cs
+++ RememberMe/Assets/Project/Scripts/Maze/UpdateMazeLevel.cs
@@ -15,6 +15,8 @@ public class UpdateMazeLevel : MonoBehaviour
     [SerializeField]
     public Text timeText;
     int countPiece;
+    [SerializeField]
+    public Image map;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +24,7 @@ public class UpdateMazeLevel : MonoBehaviour
             
             Instantiate(MazePrefabs[0], new Vector3(-39.52f, 0.0f, 16.55f), Quaternion.identity);
             npc.dialogueIndex = 1;
-            
+            map.enabled = false;
         }else if(data_Storage.level == 2 ){
             Instantiate(MazePrefabs[1], new Vector3(-51.01407f, -4.77844f, -5.782525f), Quaternion.identity);
             npc.dialogueIndex = 2;
